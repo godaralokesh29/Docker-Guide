baee76af5d681f228e9e68518f763c583db12679
godaralokesh29 <godaralokesh2023@gmail.com>
2026-06-11 22:51:20 +0530

    docker compose

---
+++ dockercompose/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+const {PrismaClient} = require("@prisma/client")
+const prisma = new PrismaClient();
+
+const app = express();
+
+app.listen(3000)
+
+app.get("/",async (_req,res)=>{
+    const data=await prisma.user.findMany();
+    console.log(data)
+    
+    
+    res.send({
+        message:"hello guys"
+    })
+})
+
+app.post("/users",async (_req,res)=>{
+    await prisma.user.create({
+       data:{
+        id:Math.random().toString(),
+        name:Math.random().toString()
+
+       }
+        
+    })
+    res.json({
+        message:"user done"
+    })
+    
+})
